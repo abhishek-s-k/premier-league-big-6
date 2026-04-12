782bc2373e51356618a247e619ebaddaadd58fed
kulkarnis.abhishek@gmail.com <kulkarnis.abhishek@gmail.com>
2026-04-12 09:11:03 +0000

    updated to incremental load

--- src/gold/match_stats_gold.py
+++ src/gold/match_stats_gold.py
@@ -5,30 +5,17 @@ import pyspark.sql.window as W
 
 # COMMAND ----------
 
-# pl_big_6.bronze.match_data
-df_match_data = spark.read.table("pl_big_6.bronze.transfer_data")
-# .where(F.to_date(F.col('created_at'))>=F.current_date())
+df_match_data = (spark.read.load("/Volumes/pl_big_6/silver/pl_data/match_data").where(F.to_date(F.col('created_at'))>=F.current_date()))
+# .where(F.col('session') == F.when(F.month(F.current_date())>=7,F.concat(F.year(F.current_date()),F.lit('-'),F.year(F.current_date())+1)).otherwise(F.concat(F.year(F.current_date())-1,F.lit('-'),F.year(F.current_date()))))
+#explore chnage data feed
 
 # COMMAND ----------
 
-display(df_match_data.count())
+df_match_data.count()
 
 # COMMAND ----------
 
-# display(df_match_data)
-df_match_stats = df_match_data.select(
-    F.coalesce(F.try_to_date(F.col("Date"),'dd/MM/yyyy'),F.try_to_date(F.col("Date"),'dd/MM/yy')).alias('Date'),
-    F.col("HomeTeam"),
-    F.col("AwayTeam"),
-    F.col("FTHG").cast('int'),
-    F.col("FTAG").cast('int'),
-    F.col("FTR")
-).distinct()
-
-
-# COMMAND ----------
-
-df_teams_table_home = df_match_stats.groupBy(F.col("HomeTeam").alias("Team")).agg(
+df_teams_table_home = df_match_data.groupBy(F.col('season'),F.col("HomeTeam").alias("Team")).agg(
     F.sum(F.col("FTHG")).alias("GH"),
     F.sum(F.col("FTAG")).alias("GCH"),
     F.count(F.lit(1)).alias("MP"),
@@ -36,10 +23,11 @@ df_teams_table_home = df_match_stats.groupBy(F.col("HomeTeam").alias("Team")).ag
         F.when(F.col("FTR") == "D", 1).when(F.col("FTR") == "A", 0).otherwise(3)
     ).alias("PointsHome"),
 )
+df_teams_table_home.display()
 
 # COMMAND ----------
 
-df_teams_table_away = df_match_stats.groupBy(F.col("AwayTeam").alias("Team")).agg(
+df_teams_table_away = df_match_data.groupBy(F.col('season'),F.col("AwayTeam").alias("Team")).agg(
     F.sum(F.col("FTAG")).alias("GA"),
     F.sum(F.col("FTHG")).alias("GCA"),
     F.count(F.lit(1)).alias("MP"),
@@ -47,6 +35,7 @@ df_teams_table_away = df_match_stats.groupBy(F.col("AwayTeam").alias("Team")).ag
         F.when(F.col("FTR") == "D", 1).when(F.col("FTR") == "H", 0).otherwise(3)
     ).alias("PointsAway"),
 )
+
 df_teams_table = (
     df_teams_table_home.join(
         df_teams_table_away,
@@ -59,6 +48,7 @@ df_teams_table = (
 ).select(
     df_teams_table_home.Team,
     (df_teams_table_home.MP + df_teams_table_away.MP).alias("MP"),
+    df_teams_table_home.season,
     "Points",
     "PointsHome",
     "PointsAway",
@@ -73,6 +63,19 @@ df_teams_table = (
 
 # COMMAND ----------
 
-df_teams_table.write.format('delta')\
-    .mode('overwrite')\
-    .saveAsTable('pl_big_6.silver.match_stats')
+seasons = df_teams_table.selectExpr("season").distinct().collect()
+list_of_seasons= [row[0] for row in seasons]
+# for i in seasons:
+#     list_of_seasons.append(i)
+condition = 'season in ('+",".join(f"'{i}'" for i in list_of_seasons)+')'
+print(condition)
+
+# COMMAND ----------
+
+(df_teams_table.write.format('delta')
+    # .partitionBy("season")
+    .mode('overwrite')
+    .option("replaceWhere",condition)
+    # .save('/Volumes/pl_big_6/gold/match_data/pl_match_data')
+    .saveAsTable('pl_big_6.gold.match_data')
+)
